48edf1dd6be186c0c38d955ceb3ef94e76bf6041
Mateus <mateuspv2003@gmail.com>
2023-09-04 20:19:32 -0300

    Fazendo ajustes no curriculo

---
+++ script.js
@@ -0,0 +1,20 @@
+divExperiencias = document.querySelector["#experiencies"]
+
+experiencias = [
+    {
+        "empresa" : "empresa Y",
+        "cargo" : "",
+        "descricao-cargo" : "",
+
+    }
+]
+
+divExperience = document.createElement("div")
+divExperience.setAttribute("class", "experience")
+
+empresa = document.createElement("h3")
+empresa.setAttribute("class", "empresa")
+empresa.innerText = experiencias[0].empresa
+
+divExperience.appendChild(empresa)
+divExperiencias.appendChild(divExperience)
